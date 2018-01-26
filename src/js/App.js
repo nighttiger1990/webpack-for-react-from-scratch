cf18bd6b5e09659c65f2a610b26a49bedcf2c4d1
TruongBL <truongbl@topica.edu.vn>
2018-01-26 11:05:56 +0700

    Add folder dist to gitignore and delete from source

--- src/js/App.js
+++ src/js/App.js
@@ -1,1 +1,25 @@
-import FormContainer from "./components/containers/FormContainer"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Input from './components/presentationals/Input';
+import FormContainer from './components/containers/FormContainer';
+export default class App extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (<div class="row mt-5">
+            <div class="col-md-4 offset-md-1">
+                <p>Create a new article! This is very cool</p>
+                <div id="create-article-form">
+                    <FormContainer />
+                </div>
+
+            </div>
+
+        </div>
+        )
+    }
+}
+
+const wrapper = document.getElementById("root");
+wrapper? ReactDOM.render(<App/>, wrapper) : false
